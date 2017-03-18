724523c93b077093eab97f6f6e44299efa7002e0
Dong Chen <dongchen@CNdongchen.local>
2017-03-18 17:15:58 +0800

    add the test for wx messager helper

---
+++ test/helper/wx-message-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const { wxMessageParser } = require('../../helper/wx-message');
+
+const wxMessage = require('../fixture/wxAppMessage');
+const result = require('../fixture/result');
+
+
+describe('wxMessageParser', ()=>{
+   it('should parse the wx message', (done)=> {
+       wxMessageParser(wxMessage).then(
+           messageObj => {
+               expect(JSON.stringify(messageObj)).to.equal(result);
+               done();
+           }
+       ).catch(done);
+   })
+});
